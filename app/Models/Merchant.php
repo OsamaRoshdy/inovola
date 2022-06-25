c8bd5e736a97be82d36f6c35081c4407d5277fae
Osama Roshdy <osama.roshdy@tqinat.com>
2022-06-25 16:35:58 +0200

    initializing database schema

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -2,25 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Merchant extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'shop_name', 'is_products_include_vat'];
 
     protected $hidden = ['password', 'remember_token'];
 
     protected $casts = ['email_verified_at' => 'datetime'];
 
-    public function cart()
+    public function products()
     {
-        return $this->hasOne(Cart::class);
+        return $this->hasMany(Product::class);
     }
 
 }
---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Product extends Model
+{
+    use HasFactory, HasTranslations;
+
+    protected $fillable = ['name', 'description', 'price', 'merchant_id'];
+
+    public $translatable = ['name', 'description'];
+
+}
